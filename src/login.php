1c2136975bd248b66fea0b202a32ff82c764b812
TuriImfura <alexanderturiimfura@gmail.com>
2024-03-20 21:40:08 +0200

    login page is created

---
+++ src/login.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login&Register Module</title>
+</head>
+<body>
+    <div>
+        <h3>Login Form</h3>
+        <form method="post" action="">
+            Email<br/>
+            <input type="email" name="email" placeholder="Enter Email" /><br/>
+            Password<br/>
+            <input type="text" name="password" placeholder="Enter password" /><br/>
+            <input type="submit" value="Login" /><br/>
+        </form>
+    </div>
+</body>
+</html>
