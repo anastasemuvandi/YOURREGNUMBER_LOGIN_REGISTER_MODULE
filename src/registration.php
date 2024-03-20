c5d3edd5b5ee7feb685be9c4b3fab53518451a91
TuriImfura <alexanderturiimfura@gmail.com>
2024-03-20 21:35:45 +0200

    registration page is created

---
+++ src/registration.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login&Register Module</title>
+</head>
+<body>
+    <div>
+        <h3>Registration Form</h3>
+        <form method="post" action="">
+            First name<br/>
+            <input type="text" name="fname" placeholder="First Name" /><br/>
+            Last name<br/>
+            <input type="text" name="lname" placeholder="Last Name" /><br/>
+            Email<br/>
+            <input type="email" name="email" placeholder="Enter Email" /><br/>
+            Password<br/>
+            <input type="text" name="password" placeholder="Enter password" /><br/>
+            <input type="submit" value="Register" /><br/>
+        </form>
+    </div>
+</body>
+</html>
